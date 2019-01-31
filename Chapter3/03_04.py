2d30e8c643ae30e457eca80026bc505bfd3cdebc
Vishesh <vishesh2k6@gmail.com>
2019-01-31 16:59:27 -0600

    Create 03_04.py

---
+++ Chapter3/03_04.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('faces.jpeg',1)
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# Split into hue, saturation and intensity channels
+h = hsv[:,:,0]
+s = hsv[:,:,1]
+v = hsv[:,:,2]
+
+hsv_split = np.concatenate((h,s,v), axis=1)
+cv2.imshow("Split HSV",hsv_split)
+
+# Filter on saturation channel
+ret, min_sat = cv2.threshold(s,40,255, cv2.THRESH_BINARY)
+cv2.imshow("Sat Filter",min_sat)
+
+# Reverse filter on hue channel
+ret, max_hue = cv2.threshold(h,15, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow("Hue Filter",max_hue)
+
+# Combine bothe filters - analogos to multiplying their effects
+final = cv2.bitwise_and(min_sat,max_hue)
+cv2.imshow("Final",final)
+cv2.imshow("Original",img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
